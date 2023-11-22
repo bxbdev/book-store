dd1217012fd08c2d90542a3a3a48450cb753a5ef
Fiona <workartsources@gmail.com>
2023-11-22 10:54:28 +0800

    feat: added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        source: "var(--source)",
+      },
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        bold: 900,
+      },
+      colors: {
+        primary: "#F96587",
+      },
+    },
   },
   plugins: [],
 };
